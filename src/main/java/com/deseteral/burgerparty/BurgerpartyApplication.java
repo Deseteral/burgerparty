d5da6f5ca08f13d7e3bfe82a8fd3493769ba3065
Michał Ferchow <deseteral@gmail.com>
2017-04-20 23:58:13 +0200

    Initial commit

---
+++ src/main/java/com/deseteral/burgerparty/BurgerpartyApplication.java
@@ -0,0 +1,12 @@
+package com.deseteral.burgerparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerpartyApplication.class, args);
+	}
+}
